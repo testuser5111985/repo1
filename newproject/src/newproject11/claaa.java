60ead2fde01015dea2d6e6a08620287edb2baf9b
smrati.rai <smrati.rai@LPCLH-5CD8032ZQW.espire.com>
2019-01-22 23:01:29 +0530

    first

---
+++ newproject/src/newproject11/claaa.java
@@ -0,0 +1,15 @@
+package newproject11;
+
+public class claaa {
+	static int count=5;
+	public static void main(String[] args) {
+		for(int i=0;i<=2;i++) {
+		System.out.println("test");
+		count++;
+		System.out.println(count);
+		}
+		
+
+	}
+
+}
